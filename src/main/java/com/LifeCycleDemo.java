f445f23212490012528ebc05678a4d3b2e839bbb
Ehtesham <ehteshamuddinqureshi.mohd@zemosolabs.com>
2022-01-27 18:47:14 +0530

    Complete Demo

--- src/main/java/com/LifeCycleDemo.java
+++ src/main/java/com/LifeCycleDemo.java
@@ -5,9 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class LifeCycleDemo {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("lifeCycleContext.xml");
-       Coach fbCoach =  context.getBean("myCoach" , Coach.class);
-        System.out.println(fbCoach.getCurrentWorkOut());
-        context.close();
+            ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("lifeCycleContext.xml");
+           Coach fbCoach =  context.getBean("myCoach" , Coach.class);
+            System.out.println(fbCoach.getCurrentWorkOut());
+            context.close();
     }
 }
